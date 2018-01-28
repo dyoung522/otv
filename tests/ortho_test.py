4f3eee37eeb109c5975762f798b59afbf4015acb
Donovan C. Young <dyoung522@gmail.com>
2018-01-27 22:15:09 -0500

    refactor tests

--- tests/ortho_test.py
+++ tests/ortho_test.py
@@ -2,8 +2,8 @@ import pytest
 from ovt.ortho import Tile
 
 lat = "+11"
-long = "+222"
-tile_name = f"zOrtho{lat}{long}"
+long = "-222"
+tile_name = f"zOrtho_{lat}{long}"
 
 
 @pytest.fixture
@@ -11,24 +11,20 @@ def tile():
     return Tile(tile_name)
 
 
-def test_tile_name():
-    assert (tile().tile_name) == tile_name
+def test_tile_name(tile):
+    assert (tile.tile_name) == tile_name
 
 
-def test_tile_lat():
-    assert (tile().lat) == lat
+def test_tile_lat(tile):
+    assert (tile.lat) == lat
 
 
-def test_tile_long():
-    assert (tile().long) == long
+def test_tile_long(tile):
+    assert (tile.long) == long
 
 
-def test_directories_returns_a_list():
-    assert type(tile().directories()) is list
-
-
-def test_directories_returns_valid_items():
-    assert len(tile().directories()) is 3
-
+def test_tile_directories(tile):
+    assert type(tile.directories()) is list
+    assert len(tile.directories()) is 3
     for dir in ["earth nav data", "textures", "terrain"]:
-        assert dir in tile().directories(), "Did not find \"{}\" in list of directories to check".format(dir)
+        assert dir in tile.directories(), "Did not find \"{}\" in list of directories to check".format(dir)
--- tests/util_test.py
+++ tests/util_test.py
@@ -1,13 +1,11 @@
+import pytest
 from ovt.util import *
 
 
-def test_pluralize_with_zero():
-    assert (pluralize(0, "person", "people")) == "no people"
-
-
-def test_pluralize_with_one():
-    assert (pluralize(1, "person", "people")) == "1 person"
-
-
-def test_pluralize_with_two():
-    assert (pluralize(2, "person", "people")) == "2 people"
+@pytest.mark.parametrize("count, singular, plural, expect", [
+    (0, "person", "people", "no people"),
+    (1, "person", "people", "1 person"),
+    (2, "person", "people", "2 people"),
+])
+def test_pluralize(count, singular, plural, expect):
+    assert pluralize(count, singular, plural) == expect
